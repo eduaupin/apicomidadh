eb81b7eb163f251a555739fb914dc58d667d5f30
thaisacamargo <thais.camargo.ta@gmail.com>
2019-10-05 11:30:12 -0300

    Correção dos ícones e adi~ção do fluxo novo evento

--- app/src/main/java/com/example/views/eventos/ListaEventosActivity.java
+++ app/src/main/java/com/example/views/eventos/ListaEventosActivity.java
@@ -1,34 +1,53 @@
 package com.example.views.eventos;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.support.annotation.NonNull;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 
 import com.example.login.R;
 
 public class ListaEventosActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
-
+    private FloatingActionButton btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_eventos);
 
+        initViews();
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                criarEvento();
+            }
+        });
+
+
+
         toolbar = findViewById(R.id.my_toolbar);
-        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back_black_24dp));
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow));
         toolbar.setTitle("Eventos");
         toolbar.setTitleTextColor(getResources().getColor(android.R.color.black));
         setSupportActionBar(toolbar);
 
+    }
+
+
+    private void initViews(){
+        btnAdd = findViewById(R.id.floatingActionButton3);
+    }
+
+    private void criarEvento(){
+        startActivity(new Intent(ListaEventosActivity.this,CriarEventoActivity.class));
     }
 
     @Override
