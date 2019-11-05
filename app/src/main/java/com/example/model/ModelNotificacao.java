961e394f5a2ec6c4921f8a283246deb65c6d635b
thaisacamargo <thais.camargo.ta@gmail.com>
2019-11-04 23:49:21 -0300

    Implementação recycler notificacoes(adapter, model, item), melhorias de layout activity detalhes do prato

---
+++ app/src/main/java/com/example/model/ModelNotificacao.java
@@ -0,0 +1,60 @@
+package com.example.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ModelNotificacao implements Parcelable {
+
+    private String title;
+    private String date;
+
+    public ModelNotificacao(String title, String date) {
+        this.title = title;
+        this.date = date;
+    }
+
+    protected ModelNotificacao(Parcel in) {
+        title = in.readString();
+        date = in.readString();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
+    public static final Creator<ModelNotificacao> CREATOR = new Creator<ModelNotificacao>() {
+        @Override
+        public ModelNotificacao createFromParcel(Parcel in) {
+            return new ModelNotificacao(in);
+        }
+
+        @Override
+        public ModelNotificacao[] newArray(int size) {
+            return new ModelNotificacao[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(title);
+        parcel.writeString(date);
+    }
+}
--- app/src/main/java/com/example/views/home/NotificacoesActivity.java
+++ app/src/main/java/com/example/views/home/NotificacoesActivity.java
@@ -1,23 +1,30 @@
 package com.example.views.home;
 
-import android.content.Intent;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.login.R;
+import com.example.model.ModelNotificacao;
+import com.example.views.adapter.NotificacaoAdapter;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotificacoesActivity extends AppCompatActivity {
 
     public TextView txtVer;
+    private RecyclerView recyclerView;
+    private NotificacaoAdapter adapter;
+    private List<ModelNotificacao> listaNotificacoes = new ArrayList<>();
 
+    public static final String NOTIFICACAO_KEY = "Notificacao";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,7 @@ public class NotificacoesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_notificacoes);
 
         txtVer = findViewById(R.id.txtVerMais);
+        recyclerView = findViewById(R.id.recyclerViewNotificacao);
 
         Toolbar notToolbar = findViewById(R.id.toolbar_notificacoes);
         setSupportActionBar(notToolbar);
@@ -37,12 +45,29 @@ public class NotificacoesActivity extends AppCompatActivity {
 
         //TODO: Implementar layout do item e lógica para notificações
 
+        adapter = new NotificacaoAdapter(getNotificacoes());
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         txtVer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(txtVer, "Função Não Disponível!", Snackbar.LENGTH_LONG).show();
             }
         });
+
+    }
+
+    public List<ModelNotificacao> getNotificacoes() {
+        listaNotificacoes.add(new ModelNotificacao("Notificação 1", "01/01/2019"));
+        listaNotificacoes.add(new ModelNotificacao("Notificação 2", "01/02/2019"));
+        listaNotificacoes.add(new ModelNotificacao("Notificação 3", "01/03/2019"));
+        listaNotificacoes.add(new ModelNotificacao("Notificação 4", "01/04/2019"));
+        listaNotificacoes.add(new ModelNotificacao("Notificação 5", "01/05/2019"));
+        listaNotificacoes.add(new ModelNotificacao("Notificação 6", "01/06/2019"));
+
+        return listaNotificacoes;
 
     }
 
