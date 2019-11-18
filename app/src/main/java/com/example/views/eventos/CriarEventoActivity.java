d229cdbbe0a4b4783545af05064754d56cc77bc4
Nina Lofrese <nina.lofrese@gmail.com>
2019-11-18 00:07:47 -0300

    Layout tela de novo evento parcialmente concluído

--- app/src/main/java/com/example/views/eventos/CriarEventoActivity.java
+++ app/src/main/java/com/example/views/eventos/CriarEventoActivity.java
@@ -10,39 +10,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.login.R;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class CriarEventoActivity extends AppCompatActivity {
     private Toolbar toolbar;
-    private Button btnSalvar;
-    private Button btnAdicionarParticipante;
-    private Button btnAdicionarPrato;
+    private TextInputLayout inputTitulo;
+    private TextInputLayout inputData;
+    private TextInputLayout inputHora;
+    private TextInputLayout inputLocal;
+    private Button buttonAdicionarParticipantes;
+    private Button buttonAdicionarPratos;
+    private MaterialButton buttonSalvarEvento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criar_evento);
 
-        btnSalvar = findViewById(R.id.button_salvar);
-        btnAdicionarParticipante = findViewById(R.id.btnAddParticipante);
-        btnAdicionarPrato = findViewById(R.id.btnAddPratos);
+        initViews();
 
         //TODO: Será que não conseguimos usar um datepicker para data e hora?
 
-        toolbar = findViewById(R.id.toolbarNovoEvento);
-        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow));
-        toolbar.setTitle("Criar Evento");
-        toolbar.setTitleTextColor(getResources().getColor(android.R.color.black));
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Novo Evento");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setElevation(0);
 
-        btnAdicionarParticipante.setOnClickListener(view -> {
+        buttonAdicionarParticipantes.setOnClickListener(view -> {
             startActivity(new Intent(CriarEventoActivity.this, AdicionarPessoaAoEventoActivity.class));
         });
 
-        btnAdicionarPrato.setOnClickListener(view -> {
+        buttonAdicionarPratos.setOnClickListener(view -> {
             //TODO: Adicionar tela de adicionar prato em evento (searchview com recycler)
-            Snackbar.make(btnAdicionarParticipante, "Função não disponível", Snackbar.LENGTH_SHORT).show();
+            Snackbar.make(buttonSalvarEvento, "Função não disponível", Snackbar.LENGTH_SHORT).show();
         });
+    }
+
+    private void initViews() {
+        toolbar = findViewById(R.id.toolbar_novo_evento);
+        inputTitulo = findViewById(R.id.input_novo_evento_titulo);
+        inputData = findViewById(R.id.input_novo_evento_data);
+        inputHora = findViewById(R.id.input_novo_evento_hora);
+        inputLocal = findViewById(R.id.input_novo_evento_local);
+        buttonAdicionarParticipantes = findViewById(R.id.button_novo_participante);
+        buttonAdicionarPratos = findViewById(R.id.button_novo_pratos);
+        buttonSalvarEvento = findViewById(R.id.button_novo_evento_salvar);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
     }
 
     @Override
