171b1ee89c6827292e9120612dd01b7cd77fd248
Eduardo Pinheiro <eduardo.pinheiro@jussi.local>
2019-11-30 12:18:51 -0300

    adicionando registro por e-mail e Google

--- app/src/main/java/br/com/digitalhouse/foodparty/views/login/LoginActivity.java
+++ app/src/main/java/br/com/digitalhouse/foodparty/views/login/LoginActivity.java
@@ -1,17 +1,35 @@
 package br.com.digitalhouse.foodparty.views.login;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.login.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+import br.com.digitalhouse.foodparty.util.AppUtil;
 import br.com.digitalhouse.foodparty.util.BlurUtil;
 
 import br.com.digitalhouse.foodparty.views.home.HomeActivity;
@@ -30,6 +48,11 @@ public class LoginActivity extends AppCompatActivity {
     private TextView txtRegistrese;
 
 
+    private SignInButton googleSignInButton;
+    private GoogleSignInClient googleSignInClient;
+    public static final String GOOGLE_ACCOUNT = "google_account";
+
+
     private static final String EMAIL_PATTERN = "^[a-zA-Z0-9#_~!$&'()*+,;=:.\"(),:;<>@\\[\\]\\\\]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*$";
     private Pattern pattern = Pattern.compile(EMAIL_PATTERN);
     private Matcher matcher;
@@ -46,6 +69,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
+
         //Add efeito na imagem de background
         ImageView bgRegister = (ImageView) findViewById(R.id.image_register_background);
         Bitmap bitmap = ((BitmapDrawable) bgRegister.getDrawable()).getBitmap();
@@ -53,12 +77,9 @@ public class LoginActivity extends AppCompatActivity {
 
         initViews();
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                validarCampos();
-            }
-        });
+        loginGoogle();
+
+        btnLogin.setOnClickListener(view -> validarCampos());
 
         btnFacebook.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,7 +105,33 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    public void loginEmail() {
 
+        String email = inputEmail.getEditText().getText().toString();
+        String password = inputSenha.getEditText().getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Campos não podem ser vazios :(", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        FirebaseAuth.getInstance()
+                .signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+
+                    if(task.isSuccessful()){
+
+                        irParaHome(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                    }else {
+
+                        Snackbar.make(btnLogin, task.getException().getMessage(), Snackbar.LENGTH_SHORT).show();
+
+                    }
+
+                });
+
+    }
     public void initViews() {
         inputEmail = findViewById(R.id.tilayout_login_email);
         inputSenha = findViewById(R.id.tilayout_login_password);
@@ -92,6 +139,14 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.button_login);
         btnFacebook = findViewById(R.id.button_login_facebook);
         txtRegistrese = findViewById(R.id.text_login_register);
+        googleSignInButton = findViewById(R.id.sign_in_button);
+
+    }
+
+    private void irParaHome(String uiid) {
+        AppUtil.salvarIdUsuario(getApplication().getApplicationContext(), uiid);
+        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+        finish();
     }
 
     public void validarCampos(){
@@ -133,5 +188,62 @@ public class LoginActivity extends AppCompatActivity {
 
     public void esqueciSenha(){
         startActivity(new Intent(LoginActivity.this, EsqueciSenhaActivity.class));
+    }
+
+    public void concluirLogin(GoogleSignInAccount conta) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        intent.putExtra(GOOGLE_ACCOUNT, conta);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK){
+            switch (requestCode){
+                case 101:
+                    try{
+                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+                        GoogleSignInAccount conta = task.getResult(ApiException.class);
+                        concluirLogin(conta);
+
+                    }catch (ApiException e){
+                        Log.i("LOG", "Error: " + e.getMessage());
+
+                        Toast.makeText(getApplicationContext(), "Erro", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        GoogleSignInAccount alreadyLoggedAccount =GoogleSignIn.getLastSignedInAccount(this);
+        if(alreadyLoggedAccount != null){
+            Toast.makeText(this, "Você já está logadp", Toast.LENGTH_SHORT).show();
+            concluirLogin(alreadyLoggedAccount);
+        }else{
+            Toast.makeText(this, "Faça o login no app :)", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void loginGoogle(){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        googleSignInButton.setOnClickListener(view -> {
+            Intent signIntent = googleSignInClient.getSignInIntent();
+            startActivityForResult(signIntent, 101);
+        });
+
     }
 }
