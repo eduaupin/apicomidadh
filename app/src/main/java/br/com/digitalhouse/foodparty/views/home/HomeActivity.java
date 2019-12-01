f2655b15224c917c17dc2871e456e2fde0ab37e0
thaisacamargo <thais.camargo.ta@gmail.com>
2019-12-01 14:27:52 -0300

    Atualizando 'Ver Mais Pratos', removendo search bar, alterando botão back, removendo botão notificações da Home

--- app/src/main/java/br/com/digitalhouse/foodparty/views/home/HomeActivity.java
+++ app/src/main/java/br/com/digitalhouse/foodparty/views/home/HomeActivity.java
@@ -109,26 +109,6 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        int id = item.getItemId();
-
-        if (id == R.id.menu_notificacao) {
-
-            startActivity(new Intent(HomeActivity.this, NotificacoesActivity.class));
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void replaceFragment(Fragment fragment) {
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
--- app/src/main/java/br/com/digitalhouse/foodparty/views/pratos/ListaDePratosActivity.java
+++ app/src/main/java/br/com/digitalhouse/foodparty/views/pratos/ListaDePratosActivity.java
@@ -2,26 +2,89 @@ package br.com.digitalhouse.foodparty.views.pratos;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.login.R;
 
-public class ListaDePratosActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.digitalhouse.foodparty.model.Prato;
+import br.com.digitalhouse.foodparty.viewmodel.HomeFragmentViewModel;
+import br.com.digitalhouse.foodparty.views.adapter.CardFavoritosAdapter;
+import br.com.digitalhouse.foodparty.views.adapter.PratosPopularesAdapter;
+import br.com.digitalhouse.foodparty.views.interfaces.ClickEvento;
+import br.com.digitalhouse.foodparty.views.interfaces.ClickPratos;
+
+import static br.com.digitalhouse.foodparty.views.home.HomeFragment.PRATO_KEY;
+
+public class ListaDePratosActivity extends AppCompatActivity implements ClickPratos {
     private Toolbar toolbar;
+    private RecyclerView recyclerViewPratos;
+    private PratosPopularesAdapter pratosAdapter;
+    private List<Prato> listaPratosPopulares = new ArrayList<>();
+    private HomeFragmentViewModel viewModel;
+    private ProgressBar loadingPratos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_de_pratos);
 
+        initViews();
+        setupToolbar();
+        setupRecycler();
+        buscaDados();
+
+    }
+
+    private void initViews() {
         toolbar = findViewById(R.id.my_toolbar);
-        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back_black_24dp));
-        toolbar.setTitle("Pratos");
+        recyclerViewPratos = findViewById(R.id.recycler_lista_de_pratos);
+        pratosAdapter = new PratosPopularesAdapter(listaPratosPopulares, this);
+        viewModel = ViewModelProviders.of(this).get(HomeFragmentViewModel.class);
+        loadingPratos = findViewById(R.id.progress_bar_home);
+    }
+
+    private void setupToolbar(){
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow));
+        toolbar.setTitle("Pratos Populares");
         toolbar.setTitleTextColor(getResources().getColor(android.R.color.black));
         setSupportActionBar(toolbar);
+    }
 
+    private void setupRecycler(){
+        recyclerViewPratos.setAdapter(pratosAdapter);
+        GridLayoutManager gdManager = new GridLayoutManager(this, 2);
+        recyclerViewPratos.setLayoutManager(gdManager);
+    }
+
+    private void buscaDados(){
+        viewModel.getPratos();
+        viewModel.getPratosLiveData().observe(this, pratos -> {
+            pratosAdapter.atualizaLista(pratos);
+        });
+
+        viewModel.getLoading().observe(this, loading -> {
+            if (loading) {
+                loadingPratos.setVisibility(View.VISIBLE);
+            } else {
+                loadingPratos.setVisibility(View.GONE);
+            }
+        });
     }
 
     @Override
@@ -32,4 +95,13 @@ public class ListaDePratosActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onClick(Prato prato) {
+        //Envio do objeto para a tela de detalhe
+        Intent intent = new Intent(this, DetalhesDoPratoActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(PRATO_KEY, prato);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
 }
